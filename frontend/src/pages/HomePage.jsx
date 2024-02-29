e917fdb9ce73e254237db23459f2bef0a9504644
PremrajChavan <premrajchavan0@gmail.com>
2024-02-29 23:52:20 +0530

    UI Design Done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
 import SortRepos from "../components/SortRepos";
+import Spinner from "../components/Spinner";
 
 
 const HomePage = () => {
@@ -12,7 +13,7 @@ const HomePage = () => {
 			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
 				<ProfileInfo />
 				<Repos />
-				{/* <Spinner /> */}
+				<Spinner />
 			</div>
 		</div>
 	);
